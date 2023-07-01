2ad43053625f97124d62b21ddc02cdec4dc9ad86
Suravarapu Prathyusha <SU40083948@wipro.com>
2023-07-01 12:25:24 +0530

    Updates

---
+++ precache-manifest.1555f2ff41ae694bef2f79a435852598.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80c15627496989156fc9f21406a883e",
+    "url": "/OnlineEditor/index.html"
+  },
+  {
+    "revision": "97ca95ef1a7cf3f090ea",
+    "url": "/OnlineEditor/static/css/2.8e69e9c3.chunk.css"
+  },
+  {
+    "revision": "68b01dd5a88a5454ae7b",
+    "url": "/OnlineEditor/static/css/main.792b0489.chunk.css"
+  },
+  {
+    "revision": "97ca95ef1a7cf3f090ea",
+    "url": "/OnlineEditor/static/js/2.36d467a3.chunk.js"
+  },
+  {
+    "revision": "38d5cc1cb1001b9cc38b06e2da080c18",
+    "url": "/OnlineEditor/static/js/2.36d467a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b01dd5a88a5454ae7b",
+    "url": "/OnlineEditor/static/js/main.fa750498.chunk.js"
+  },
+  {
+    "revision": "820a2cb3f447fed9fdd9",
+    "url": "/OnlineEditor/static/js/runtime-main.7fc36703.js"
+  },
+  {
+    "revision": "f0d92a6a17eb82f4094cd5cfe30942e8",
+    "url": "/OnlineEditor/static/media/CSS.f0d92a6a.png"
+  },
+  {
+    "revision": "88531d840fe3ef3a4f52e5122c2c30a7",
+    "url": "/OnlineEditor/static/media/JS.88531d84.png"
+  }
+]);
